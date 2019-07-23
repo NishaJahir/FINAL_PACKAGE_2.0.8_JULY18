727c0e9c4c9ffe491b59e95d7bef900abc901e56
NishaJahir <49142761+NishaJahir@users.noreply.github.com>
2019-07-23 15:52:47 +0530

    Update UpdateTransactionTable4.php

--- src/Migrations/UpdateTransactionTable4.php
+++ src/Migrations/UpdateTransactionTable4.php
@@ -19,7 +19,6 @@ use Novalnet\Models\TransactionLog;
 use Plenty\Modules\Plugin\DataBase\Contracts\Migrate;
 use Plenty\Modules\Plugin\DataBase\Contracts\DataBase;
 use Plenty\Modules\Plugin\DataBase\Contracts\Query;
-
 use Plenty\Plugin\Log\Loggable;
 /**
  * Class CreateTransactionTable
@@ -33,18 +32,7 @@ class UpdateTransactionTable4
      * @param Migrate $migrate
      */
     public function run(Migrate $migrate, TransactionLog $transactionlog)
-    {
-         
-           // $order = $database->query(TransactionLog::class);
-          //  $this->getLogger(__METHOD__)->error('NNNNN', $order);
-           //try {
-           //   $order = $database->query(TransactionLog::class)->where('id', '=', '1')->get();
-             $log = pluginApp(Illuminate\Database\Eloquent\Model::class);
-       // $migrate->updateTable(TransactionLog::class);
-            $this->getLogger(__METHOD__)->error('final', $log);
-          // } catch (\Exception $e) {
-          //    $migrate->createTable(TransactionLog::class);
-          //     $this->getLogger(__METHOD__)->error('RR', 'create');
-          // }  
+    { 
+       $migrate->updateTable(TransactionLog::class); 
     }
 }
